ed1c18f22bdd2cbdbfa05c6744bfd923d97846c5
Febrian-123 <2472039@maranatha.ac.id>
2025-06-07 17:17:07 +0700

    Febrian dropdown+page dropdown masing-masing

---
+++ dolci.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const list = document.getElementById("galleryList");
+
+  fetch("./JSON/dolci.json")
+    .then(res => res.json())
+    .then(data => {
+      for(let i = 0; i <=10; i++){
+        const kotak = document.createElement("div");
+        kotak.className = "kotak";
+        if(i==4){
+            kotak.innerHTML = `
+            <img src="${data[i].img}">
+        `;
+        }
+        else{
+            kotak.innerHTML = `
+            <img src="${data[i].img}">
+            <p>${data[i].nama}</p>
+            <div class="hover-box">
+                <div class="plus-icon"></div>
+                <div class="hover-text">${data[i].deskripsi}</div>
+            </div>
+        `;
+        }
+        list.appendChild(kotak);
+      };
+    });
+});
