26035fdd0a62ac7acdb2dd4416056666a660fcc8
Febrian-123 <2472039@maranatha.ac.id>
2025-06-15 16:53:34 +0700

    Febrian Lightmode

---
+++ guide-light.js
@@ -0,0 +1,19 @@
+
+  function setThemeBasedOnTime() {
+    const date = new Date();
+    const hour = date.getHours();
+    const isLightMode = hour >= 6 && hour < 18; 
+    if (isLightMode) {
+      document.body.classList.add('light-mode');
+      document.querySelector('link[href="home.css"]').href = "home-light.css";
+      document.querySelector('link[href="footer.css"]').href = "footer-light.css";
+      document.querySelector('link[href="guide.css"]').href = "guide-light.css";
+    } else {
+      document.body.classList.remove('light-mode');
+      document.querySelector('link[href="home-light.css"]').href = "home.css";
+      document.querySelector('link[href="footer-light.css"]').href = "footer.css";
+      document.querySelector('link[href="guide-light.css"]').href = "guide.css";
+    }
+  }
+  setThemeBasedOnTime();
+  setInterval(setThemeBasedOnTime, 360);
